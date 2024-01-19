7fe2dee591c835846f8de4114c63bd3d74505282
Cahyo Aji Nugroho <96677848+KingCrezz@users.noreply.github.com>
2024-01-19 08:04:49 +0000

    Pending changes exported from your codespace

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
+        <img src="isan.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          Iksan <span className="heart">♥️</span> Cipa
         </p>
         <p className="small">
           Edit <code>src/App.jsx</code> and save to reload.
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Belajar React JSX
           </a>
         </p>
       </header>
